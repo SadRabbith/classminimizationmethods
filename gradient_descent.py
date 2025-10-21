3d410cd59201243e84d1b3f3e9be03eaf7dd33bd
sadman raabith <sraabi1@lsu.edu>
2025-10-21 13:43:43 -0400

    gradient_descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -12,3 +12,19 @@ def f(x,y):
 def df(x,y): 
     df_dx = 2 * (x - 1) + 400 * x * (x**2 - y)
     df_dy = 200 * (y - x**2)
+
+    return df_dx , df_dy
+
+def gradient_descent_multivariate(x_init, y_init ,  learning_rate , num_iterations):
+    """
+    Performs gradient descent to minimize a multivariate function.
+    """
+    params = np.array(x_init, y_init, dtype=float) # Ensure float type
+    history = [params.copy()] # Store history of parameters
+
+    for i in range(num_iterations):
+        gradient = df(x_init, y_init)
+        params -= learning_rate * gradient
+        history.append(params.copy())
+
+    return params, history
