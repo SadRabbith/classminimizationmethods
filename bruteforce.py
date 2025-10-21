502facab26dab6132327e903a82ff3d17e182c30
sadman raabith <sraabi1@lsu.edu>
2025-10-21 13:22:51 -0400

    corrections

--- bruteforce.py
+++ bruteforce.py
@@ -8,24 +8,26 @@ import statistics
 
 #2D rosenbrock => f(x,y) = (1-x)**2 +100(y-x)**2
 
+x_values = [-2, -1 , 0, 1, 2]
+y_values = [-1, 0, 1 , 2, 3]
+
 
 def all_solutions(x_values, y_values):
-    rosen = []
+    rosen = dict()
     for x_value in x_values:
         for y_value in y_values: 
-            fun = (1 -x_value)**2 + 100(y_value - x_value**2)**2
-            rosen.append(fun) 
+            fun = (1 -x_value)**2 + 100*(y_value - x_value**2)**2
+            rosen[(x_value, y_value)] = fun
     
-    max = 0
+    max_value = float('-inf')
+    max_point = None
 
-    for values in range(len(rosen)): 
-        if max > rosen[values]: 
-            values = values+ 1 
-        elif max < rosen[values]: 
-            max = rosen[values]
-            values = values + 1 
-    print(max)
+    for key, value in rosen.items():
+        if value > max_value:
+            max_value = value
+            max_point = key
 
+    print(f"Maximum value: {max_value} at (x, y) = {max_point}")
+    return rosen
 
-
-
+all_solutions(x_values=x_values, y_values=y_values) 
